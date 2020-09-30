98d9ae2b697b469960e4704956e558e96ea92681
ChenZhiPeng <634323580@qq.com>
2020-09-30 18:40:42 +0800

    fix

--- src/components/Test/Test.vue
+++ src/components/Test/Test.vue
@@ -1,33 +1,27 @@
 <template>
-<div class="test">
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px" viewBox="0 0 100 100">
-    <line x1="50" y1="0"
-      x2="50" y2="100"
-      stroke="red"
-      stroke-width="5"/>
-    <line x1="0" y1="50"
-      x2="100" y2="50"
-      stroke="red"
-      stroke-width="5"/>
-  </svg>
-</div>
+<svg width="0" height="0" viewBox="0 0 100 100">
+  <defs>
+    <symbol id="more" viewBox="0 0 100 100">
+      <circle cx="20" cy="25" r="5" fill="currentColor" />
+      <circle cx="20" cy="50" r="5" fill="currentColor" />
+      <circle cx="20" cy="75" r="5" fill="currentColor" />
+      <line x1="40" y1="25" x2="90" y2="25" stroke="currentColor" stroke-width="4" />
+      <line x1="40" y1="50" x2="90" y2="50" stroke="currentColor" stroke-width="4" />
+      <line x1="40" y1="75" x2="90" y2="75" stroke="currentColor" stroke-width="4" />
+    </symbol>
+    <symbol id="filledArrowRight" viewBox="0 0 100 100">
+      <polyline points="20 10, 80 50, 20 90" fill="currentColor"></polyline>
+    </symbol>
+    <symbol id="arrowRight" viewBox="0 0 100 100">
+      <polyline points="20 10, 80 50, 20 90, 20 10" fill="transparent" stroke="currentColor" stroke-width="1"></polyline>
+    </symbol>
+  </defs>
+</svg>
 </template>
 
 <script>
 import { computed, ref, onMounted } from 'vue'
 export default {
   name: 'TestComponent',
-  setup() {
-    const message = 'Test1666'
-    return {
-      message,
-    }
-  }
 };
 </script>
-
-<style lang="scss" scoped>
-.test{
-  color: red;
-}
-</style>
